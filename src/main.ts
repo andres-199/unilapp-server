0a5416cfac8b2d7567fd9c1c5e11e0c249891fb7
andres199 <calopez@asertisas.com>
2021-05-17 17:59:46 -0500

    ssl certificate added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('Main');
+
+let httpsOptions: any;
+try {
+  httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/unilapp.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/unilapp.com/fullchain.pem'),
+  };
+  logger.log('Leyendo certificado SSL');
+} catch (error) {
+  logger.log('No se pudo encontrar el certificado SSL');
+}
 
 async function bootstrap() {
   const volleyball = require('volleyball');
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, { cors: true, httpsOptions });
 
   app.use(async (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
